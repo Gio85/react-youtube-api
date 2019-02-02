1cd812da1e5d5d4a50df06d9fccd5e27011b384c
Gio85 <giovanni.galiero85@gmail.com>
2019-02-02 12:57:49 +0000

    added some css and the videoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 import Youtube from '../apis/youtube';
+import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 class App extends React.Component{
 
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
+  }
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
   }
 
   onTermSubmit = async (term) => {
@@ -17,9 +23,16 @@ class App extends React.Component{
       }
     });
 
-    this.setState({ videos: response.data.items });
-    
+    this.setState({ 
+      videos: response.data.items, 
+      selectedVideo: response.data.items[0]
+    });
   };
+
+  onVideoSelect = (video) => {
+    console.log('From the App Component:', video);
+    this.setState({ selectedVideo: video });
+  }
   
   render() {
     return(
@@ -27,9 +40,22 @@ class App extends React.Component{
         <SearchBar
           onFormSubmit={this.onTermSubmit}
         />
-        <VideoList
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+
+              <VideoDetail
+                selectedVideo={this.state.selectedVideo}
+              />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
