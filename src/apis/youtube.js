6058d4481f2500b7be8234f6ff7453ff2d49d2c4
Gio85 <giovanni.galiero85@gmail.com>
2019-02-02 11:13:06 +0000

    added youtube api key and created axios.create instance

---
+++ src/apis/youtube.js
@@ -0,0 +1,11 @@
+import { API_KEY } from '../../env';
+import Axios from 'axios';
+
+export default Axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: API_KEY
+  }
+});
